b1b6b8d53327304553cdeb61a6a768626b95e9a2
hotick <79753404+hotick@users.noreply.github.com>
2022-02-03 16:49:59 +0200

    Add files via upload

---
+++ ninja jija/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1643899212,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/icon-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/sprite3-sheet2.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
